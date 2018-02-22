4b9ee7238a4c14839646742b93f57bcf9a260fca
Unknown <20harmet@elmbrookstudents.org>
2018-02-22 14:57:32 -0600

    Fixed My Test Classs

--- src/Test.java
+++ src/Test.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("File");
+		int sum=0;
 		Scanner input = new Scanner(System.in);
 		Scanner fileScan = new Scanner(input.nextLine());
 		while (fileScan.hasNext()) {
-			System.out.println(fileScan.next());
+			sum+=fileScan.nextInt();
 		}
+		System.out.println(sum);
 	}
 }
