969c605a4a856382e347cbc87e37d114da69c498
Unknown <20harmet@elmbrookstudents.org>
2018-02-22 15:52:10 -0600

    Thomas

--- src/FileIOTest2.java
+++ src/FileIOTest2.java
@@ -4,7 +4,7 @@ import java.io.IOException;
  
 import java.util.Scanner;
  
-public class FileIOTest {
+public class FileIOTest2 {
  
   public static void main(String[] args) throws IOException{
  
