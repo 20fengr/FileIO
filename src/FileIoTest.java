0861d996309782f8fa4ef0e4480e700c896b7222
Riley Feng <20fengr@elmbrookstudents.org>
2018-02-22 15:44:12 -0600

    data.txt

--- src/FileIoTest.java
+++ src/FileIoTest.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 import java.io.*;
 public class FileIoTest {
-public static void main (String [] arhs) throws IOException{
+public static void main (String [] args) throws IOException{
 	Scanner input = new Scanner(System.in);
-	System.out.println("Enter the name of your data file.");
-	Scanner fileName = new Scanner(input.nextLine());
+	System.out.println("Enter file");
+	String file = input.nextLine();
+	File inputFile = new File (file);
+	Scanner fileName = new Scanner(inputFile);
 	int sum = 0;
-	while (input.hasNext()) {
-		sum += input.nextInt();
+	while (fileName.hasNext()) {
+		sum += fileName.nextInt();
 	}
+	fileName.close();
 	System.out.println("Sum: " + sum);
 }
 }
