5a75d35769146fbe1ca9f614dae8db21cc617940
Unknown <20harmet@elmbrookstudents.org>
2018-02-22 20:07:04 -0600

    All inputs
    Prints all inputs

--- src/TestFile.java
+++ src/TestFile.java
@@ -1,18 +1,23 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TestFile {
 	public static void main(String[] args) throws IOException {
 		System.out.println("File");
+		ArrayList<Integer> inputs = new ArrayList<Integer>();
 		int sum = 0;
 		Scanner input = new Scanner(System.in);
 		String file = new String(input.nextLine());
 		File fileName = new File(file);
 		Scanner fileScan = new Scanner(fileName);
 		while (fileScan.hasNext()) {
-			sum += fileScan.nextInt();
+			int x=fileScan.nextInt();
+			inputs.add(x);
+			sum += x;
 		}
 		System.out.println(sum);
+		System.out.println(inputs);
 	}
 }
