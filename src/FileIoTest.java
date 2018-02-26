4eb00e6442d732b67d78623694982e01a180e8a3
Riley Feng <20fengr@elmbrookstudents.org>
2018-02-25 22:31:11 -0600

    Display 5th Int

--- src/FileIoTest.java
+++ src/FileIoTest.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.io.*;
 public class FileIoTest {
 public static void main (String [] args) throws IOException{
@@ -8,10 +9,17 @@ public static void main (String [] args) throws IOException{
 	File inputFile = new File (file);
 	Scanner fileName = new Scanner(inputFile);
 	int sum = 0;
+	ArrayList <Integer> Every5th = new ArrayList <Integer>();
 	while (fileName.hasNext()) {
 		sum += fileName.nextInt();
+		Every5th.add(fileName.nextInt());
 	}
 	fileName.close();
 	System.out.println("Sum: " + sum);
+	System.out.println("Every 5th Integer");
+	for (int j = 0; j < Every5th.size(); j +=5) {
+		System.out.println(j);
+	}
+	
 }
 }
